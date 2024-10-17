6ce7d9654e57291090d0353ae32d5ec2d2cfa037
Anas Alrefai <101460051+anas98re@users.noreply.github.com>
2024-10-17 13:28:31 +0300

    Update PayPalPaymentController.php

--- app/Http/Controllers/PayPalPaymentController.php
+++ app/Http/Controllers/PayPalPaymentController.php
@@ -1,1 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
+
+class PayPalPaymentController extends Controller
+{
+    /**
+     * create transaction.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createTransaction()
+    {
+        return view('transaction');
+    }
+    /**
+     * process transaction.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function processTransaction(Request $request)
+    {
+        $provider = new PayPalClient;
+        //Set the API credentials specified in the 'paypal' configuration file.
+        $provider->setApiCredentials(config('paypal'));
+        //Get access token from PayPal.
+        $paypalToken = $provider->getAccessToken();
+        $response = $provider->createOrder([
+            "intent" => "CAPTURE",
+            "application_context" => [
+                "return_url" => route('successTransaction'),
+                "cancel_url" => route('cancelTransaction'),
+            ],
+            "purchase_units" => [
+                0 => [
+                    "amount" => [
+                        "currency_code" => "USD",
+                        "value" => "1000.00"
+                    ]
+                ]
+            ]
+        ]);
+        if (isset($response['id']) && $response['id'] != null) {
+            // redirect to approve href
+            //A loop that scans the links provided in the response to find the approval link.
+            foreach ($response['links'] as $links) {
+                if ($links['rel'] == 'approve') {
+                    //Direct the user to a PayPal link to approve the payment.
+                    return redirect()->away($links['href']);
+                }
+            }
+            return redirect()
+                ->route('createTransaction')
+                ->with('error', 'Something went wrong.');
+        } else {
+            return redirect()
+                ->route('createTransaction')
+                ->with('error', $response['message'] ?? 'Something went wrong.');
+        }
+    }
+    /**
+     * success transaction.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function successTransaction(Request $request)
+    {
+        $provider = new PayPalClient;
+        //The API credentials specified in the 'paypal'
+        //configuration file are set on the created instance.
+        $provider->setApiCredentials(config('paypal'));
+        $provider->getAccessToken();
+        //Payment is taken through PayPal using the token received from the order.
+        $response = $provider->capturePaymentOrder($request['token']);
+        if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+            return redirect()
+                ->route('createTransaction')
+                ->with('success', 'Transaction complete.');
+        } else {
+            return redirect()
+                ->route('createTransaction')
+                ->with('error', $response['message'] ?? 'Something went wrong.');
+        }
+    }
+    /**
+     * cancel transaction.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function cancelTransaction(Request $request)
+    {
+        return redirect()
+            ->route('createTransaction')
+            ->with('error', $response['message'] ?? 'You have canceled the transaction.');
+    }
+}
 
